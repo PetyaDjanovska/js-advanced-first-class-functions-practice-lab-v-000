6dea2f34d87c96ec6367fa40f834ddbdca611817
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-21 20:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
 function logDriverNames(drivers){
-  drivers.forEach(driver => driver.maps(d => d.name));
+  drivers.forEach(driver => driver.map(d => d.name));
+
 }
